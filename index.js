30e7403333e48d95080016c39377f63f61e68b38
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2022-11-30 23:59:33 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+function findMatching(drivers, name){
+    const names = drivers.filter(function(driver){
+        if(driver == name || driver == name.toLowerCase()){
+            return driver;
+        }
+    })
+    return names;
+}
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+
+function fuzzyMatch(drivers, str){
+     let names = drivers.filter(function(driver){
+        if(driver.charAt(0) == str || driver.slice(0, 2) == str){
+            return driver;
+        }
+    })
+    return names;
+}
+
+console.log (fuzzyMatch(drivers, "Sa"));
+
+
+  function matchName(drivers, str){
+    const names = drivers.filter(function(driver){
+        if(driver.name == str){
+            return driver;
+        }
+    })
+    return names;
+  }
